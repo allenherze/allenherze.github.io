c6c480423ffcd13c17253fcdb191e23b2ff230ce
allenherze <joshuac6378@gmail.com>
2018-08-12 19:15:30 +0530

    Script changes

--- Javascript/mainscript.js
+++ Javascript/mainscript.js
@@ -1,2 +1,7 @@
-alert('The creator says Hi !!');
+// Emoji 1
+var hamburger = String.fromCodePoint(0x1F354);
+// Emoji 2
+var hug = String.fromCodePoint(0x1F917);
+//Welcome Message
+alert('Welcome, The creator says Hi !! '+ hug + '\nHere is a burger for you ' + hamburger);
 console.log('Hello There, myself Joshua. Welcome');
